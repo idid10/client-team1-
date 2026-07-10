a124da3cb6d154411395add6c15371a2d56a53b2
kdhye516 <kdhye05@naver.com>
2026-07-11 04:02:01 +0900

    홈버튼 컴포넌트 생성

---
+++ src/components/TeamCard.tsx
@@ -0,0 +1,25 @@
+export default function TeamStatusCard() {
+  return (
+    <section className="mt-6 rounded-[28px] bg-white px-4 py-4 shadow-[0_2px_12px_rgba(0,0,0,0.06)]">
+      {/* 제목 */}
+      <h2 className="font-['Roboto'] text-[17px] font-semibold leading-[135%] text-[#494949]">
+        우리 팀 미션 현황
+      </h2>
+
+      {/* 멤버 상태 */}
+      <div className="mt-4">
+        {/* MemberStatus */}
+      </div>
+
+      {/* 픽셀 고양이 */}
+      <div className="mt-4 flex justify-center">
+        {/* PixelCat */}
+      </div>
+
+      {/* 진행바 */}
+      <div className="mt-4">
+        {/* ProgressBar */}
+      </div>
+    </section>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,16 @@
-// src/screens/Home.tsx
 import TopBar from "../components/TopBar";
+import TeamCard from "../components/TeamCard";
+import HomeButton from "../components/HomeButton";
 
 export default function Home() {
   return (
     <div className="mx-auto flex min-h-screen w-[393px] flex-col bg-[#F8F8F8]">
-      <main className="flex-1 overflow-y-auto px-4 pb-24">
+      <main className="flex-1 overflow-y-auto px-4">
         <TopBar />
+        <TeamCard />
       </main>
+
+      <HomeButton />
     </div>
   );
 }
